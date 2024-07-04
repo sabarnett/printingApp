a0519dce24fc99f736817f2857a55c641e2873f1
Steven barnett <steve@sabarnett.co.uk>
2024-07-04 19:22:50 +0100

    Initial Commit

---
+++ printingApp/printingAppApp.swift
@@ -0,0 +1,19 @@
+//
+// File: printingAppApp.swift
+// Package: printingApp
+// Created by: Steven Barnett on 04/07/2024
+// 
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+        
+
+import SwiftUI
+
+@main
+struct printingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
